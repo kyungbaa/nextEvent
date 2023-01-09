244497e8e7f11b065198a7718b0390cd1e71f274
kyungbaa <cocoroto@naver.com>
2023-01-09 18:02:56 +0900

    동적Head 콘텐츠 추가

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import Head from 'next/head';
+// 자동으로 <head>섹샨에 nextJs가 추가해준다.
+
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
 export default function Home(props) {
   return (
     <div>
+      <Head>
+        <title>NextJs Event - Next.Js 연습</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+        {/* meta태그는 해당문서의 대한 정보인 메타 데이터를 정의할 떄 사용  */}
+      </Head>
       <EventList items={props.events} />
     </div>
   );
